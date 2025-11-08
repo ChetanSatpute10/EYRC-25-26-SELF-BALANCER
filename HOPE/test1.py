e7b03e9fa6c56d5d3651debf16c857db03f0dd2c
ChetanSatpute10 <satputec2006@gmail.com>
2025-11-09 05:09:54 +0530

    test bench 3

--- HOPE/test1.py
+++ HOPE/test1.py
@@ -7,60 +7,75 @@ class contrained_vel:
         self.min_vel = min_vel
         self.max_vel = max_vel
     
-    def speed(self):
+    def get_speed(self):
         return self._cmd_vel
     
-    def speed(self, value):
-        if value>self.max_vel:
+    def set_speed(self, value):
+        if value > self.max_vel:
             self._cmd_vel = self.max_vel
-        elif value<self.min_vel:
+        elif value < self.min_vel:
             self._cmd_vel = self.min_vel
         else:
             self._cmd_vel = value
-
-  
-
-  
+        return self._cmd_vel
 
 PI = 3.141592653589793
-def sqrt (n):
+
+def sqrt(n):
     x = n 
-    for _ in range (20):
+    for _ in range(20):
         x = (x + n / x) / 2
-        return x
+    return x
     
-def copysign(a,b):
-    return abs(a) if b>=0 else -abs(a)
+def copysign(a, b):
+    return abs(a) if b >= 0 else -abs(a)
 
 def atan(z):
-    return z/(1.0 + 0.28 * z * z)  
+    return z / (1.0 + 0.28 * z * z)  
 
-def atan2(y,x):
-    if x>0:
-        return atan(y/x)
-    elif x<0 and y>=0:
-        return atan(y/x) + PI
-    elif x<0 and y<0:
-        return atan(y/x) - PI
-    elif x==0 and y>0:
-        return PI/2
-    elif x==0 and y<0:
-        return -PI/2
+def atan2(y, x):
+    if x > 0:
+        return atan(y / x)
+    elif x < 0 and y >= 0:
+        return atan(y / x) + PI
+    elif x < 0 and y < 0:
+        return atan(y / x) - PI
+    elif x == 0 and y > 0:
+        return PI / 2
+    elif x == 0 and y < 0:
+        return -PI / 2
     else:
         return 0.0
     
 def asin(x):
-    if x<-1.0 or x>1.0:
+    if x < -1.0 or x > 1.0:
         return 0.0
-    elif x==1.0:
-        return PI/2
-    elif x==-1.0:
-        return -PI/2
+    elif x == 1.0:
+        return PI / 2
+    elif x == -1.0:
+        return -PI / 2
     else:
-        return atan(x/sqrt(1.0 - x*x))
+        return atan(x / sqrt(1.0 - x * x))
+
+def cos(x):
+    # Taylor series approximation for cosine
+    result = 1.0
+    term = 1.0
+    for n in range(1, 10):
+        term *= -x * x / ((2*n-1) * (2*n))
+        result += term
+    return result
+
+def sin(x):
+    # Taylor series approximation for sine
+    result = x
+    term = x
+    for n in range(1, 10):
+        term *= -x * x / ((2*n) * (2*n+1))
+        result += term
+    return result
 
 def euler_from_quaternion(quat):
-    global roll, pitch, yaw
     x, y, z, w = quat
     sinr_cosp = 2 * (w * x + y * z)
     cosr_cosp = 1 - 2 * (x * x + y * y)
@@ -76,118 +91,168 @@ def euler_from_quaternion(quat):
     
     return [roll, pitch, yaw]
 
-
 def apply_exponential_smoothing(old_estimate, new_measurement, blend_factor=0.025):
-   
     return old_estimate + blend_factor * (new_measurement - old_estimate)
+
+def transform_velocity_to_local_frame(velocity_global, yaw_angle):
+    """Convert global velocity to robot's local frame using rotation"""
+    vx_global = velocity_global[0]
+    vy_global = velocity_global[1]
+    
+    cos_yaw = cos(-yaw_angle)
+    sin_yaw = sin(-yaw_angle)
+    
+    # Rotate velocity vector by -yaw to get local frame
+    vx_local = vx_global * cos_yaw - vy_global * sin_yaw
+    vy_local = vx_global * sin_yaw + vy_global * cos_yaw
+    
+    return [vx_local, vy_local]
+
+def transform_angular_velocity_to_local(ang_vel_global, yaw_angle):
+    """Convert global angular velocity to robot's local frame"""
+    wx_global = ang_vel_global[0]
+    wy_global = ang_vel_global[1]
+    
+    cos_yaw = cos(-yaw_angle)
+    sin_yaw = sin(-yaw_angle)
+    
+    # Rotate angular velocity to local frame
+    wx_local = wx_global * cos_yaw - wy_global * sin_yaw
+    
+    return wx_local
+
 def sysCall_init():
     sim = require('sim')
     
-    global vel_control, updated_vel
-    vel_control = contrained_vel(-180,180)
+    global vel_control
+    vel_control = contrained_vel(-180, 180)
 
     self.body_handle = sim.getObject('/body') 
     self.left_joint_handle = sim.getObject('/left_joint')  
     self.right_joint_handle = sim.getObject('/right_joint')  
     
-    self.velocity_angular = 0.0
-    self.velocity_linear_set_point = 0.0 
-    self.yaw = 0.0  
-    self.pitch_dot_filtered = 0.0
-    self.velocity_angular_filtered = 0.0  
+    # State variables in LOCAL robot frame
+    self.pitch_local = 0.0
+    self.pitch_rate_local = 0.0
+    self.pitch_rate_filtered = 0.0
     
-    self.SMOOTHING_COEFFICIENT = 0.025 
+    self.yaw_angle = 0.0
     
-    self.pitch = 0.0
-    self.pitch_dot = 0.0
-    self.wheel_velocity = 0.0
+    # Velocity in robot's forward direction
+    self.forward_velocity_local = 0.0
+    self.forward_velocity_filtered = 0.0
+    
+    # Wheel feedback
+    self.left_wheel_vel = 0.0
+    self.right_wheel_vel = 0.0
+    self.wheel_vel_avg_filtered = 0.0
+    
+    # Target velocity (commanded by user)
+    self.target_forward_velocity = 0.0
+    
+    # Filter coefficient
+    self.ALPHA = 0.05
     
     self.sim = sim
+    print("Bot initialized - Using LOCAL frame measurements")
     
 def sysCall_sensing(): 
-    global vel_control, updated_vel
-    quat = self.sim.getObjectQuaternion(self.body_handle, -1)
-    euler = euler_from_quaternion(quat)
+    # Get orientation in GLOBAL frame
+    orientation_global = self.sim.getObjectOrientation(self.body_handle, -1)
+    roll_global = orientation_global[0]
+    pitch_global = orientation_global[1]
+    self.yaw_angle = orientation_global[2]
     
-    self.pitch = euler[0]
+    # Get velocities in GLOBAL frame
+    lin_vel_global, ang_vel_global = self.sim.getObjectVelocity(self.body_handle)
     
-    lin_vel, ang_vel = self.sim.getObjectVelocity(self.body_handle)
-    self.pitch_dot = ang_vel[0] 
-    self.wheel_velocity = updated_vel
-
-    message,data,data2 = sim.getSimulatorMessage()
-
-    if (message == sim.message_keypress):
-        if (data[0]==2007): # forward up arrow
-           forward_vel =  self.velocity_linear_set_point 
-
-        elif (data[0]==2008): # backward down arrow
-           backward_vel =  self.velocity_linear_set_point
-
-        #  if (data[0]==2009): # left arrow key
-
-
-        # if (data[0]==2010): # right arrow key
-
-    else:
-        forward_vel = 0.0
-        backward_vel = 0.0
+    # === CRITICAL: Transform to LOCAL robot frame ===
+    # Convert linear velocity from global to local frame
+    vel_local = transform_velocity_to_local_frame(lin_vel_global, self.yaw_angle)
+    self.forward_velocity_local = vel_local[0]  # Forward velocity in robot's frame
+    
+    # Convert angular velocity from global to local frame
+    self.pitch_rate_local = transform_angular_velocity_to_local(ang_vel_global, self.yaw_angle)
+    
+    # Calculate pitch in LOCAL frame (pitch around robot's X-axis)
+    # This compensates for yaw rotation
+    cos_yaw = cos(self.yaw_angle)
+    sin_yaw = sin(self.yaw_angle)
+    
+    self.pitch_local = atan2(
+        sin(roll_global) * cos_yaw + sin(pitch_global) * sin_yaw,
+        cos(roll_global) * cos(pitch_global)
+    )
+    
+    # Read wheel velocities
+    self.left_wheel_vel = self.sim.getJointVelocity(self.left_joint_handle)
+    self.right_wheel_vel = self.sim.getJointVelocity(self.right_joint_handle)
 
 def sysCall_actuation():
-
+    global vel_control
     
-    global vel_control, updated_vel
-    self.pitch_dot_filtered = apply_exponential_smoothing(
-        self.pitch_dot_filtered, 
-        self.pitch_dot, 
-        self.SMOOTHING_COEFFICIENT
+    # Apply smoothing to LOCAL frame measurements
+    self.pitch_rate_filtered = apply_exponential_smoothing(
+        self.pitch_rate_filtered, 
+        self.pitch_rate_local, 
+        self.ALPHA
     )
     
-    self.velocity_angular_filtered = apply_exponential_smoothing(
-        self.velocity_angular_filtered,
-        self.wheel_velocity,
-        self.SMOOTHING_COEFFICIENT
+    # Smooth forward velocity in local frame
+    self.forward_velocity_filtered = apply_exponential_smoothing(
+        self.forward_velocity_filtered,
+        self.forward_velocity_local,
+        self.ALPHA
     )
-    velocity_linear_error = self.velocity_linear_set_point - self.velocity_angular_filtered * WHEEL_RADIUS
     
-    x1 = 0 - self.pitch 
-    x2 = self.pitch_dot_filtered
-    x3 = 0 
-    x4 = velocity_linear_error
+    # Average and smooth wheel velocities
+    avg_wheel_vel = (self.left_wheel_vel + self.right_wheel_vel) / 2.0
+    self.wheel_vel_avg_filtered = apply_exponential_smoothing(
+        self.wheel_vel_avg_filtered,
+        avg_wheel_vel,
+        self.ALPHA
+    )
     
-    lqr_v = LQR_K[0] * x1 + LQR_K[1] * x2 + LQR_K[2] * x3 + LQR_K[3] * x4
+    # === VELOCITY CONTROL ===
+    # Target is zero (no drift) - can be changed to commanded velocity later
+    self.target_forward_velocity = 0.0
     
-    wheel_vel = -lqr_v / WHEEL_RADIUS
-
-    vel_control.speed = wheel_vel   
-    updated_vel = vel_control.speed 
-
-    self.sim.setJointTargetVelocity(self.left_joint_handle, updated_vel)
-    self.sim.setJointTargetVelocity(self.right_joint_handle, updated_vel)
+    # Calculate velocity error in robot's LOCAL frame
+    velocity_error = self.target_forward_velocity - (self.wheel_vel_avg_filtered * WHEEL_RADIUS)
+    
+    # === LQR CONTROL (operates in LOCAL frame) ===
+    x1 = 0.0 - self.pitch_local         # Pitch error from upright
+    x2 = -self.pitch_rate_filtered      # Pitch angular velocity
+    x3 = 0.0                            # Unused state
+    x4 = velocity_error                 # Forward velocity error
+    
+    # LQR feedback control
+    lqr_control_signal = (LQR_K[0] * x1 + 
+                          LQR_K[1] * x2 + 
+                          LQR_K[2] * x3 + 
+                          LQR_K[3] * x4)
+    
+    # Convert to wheel velocity command
+    wheel_cmd = -lqr_control_signal / WHEEL_RADIUS
+    
+    # Constrain output
+    wheel_cmd_final = vel_control.set_speed(wheel_cmd)
+    
+    # Apply to both motors
+    self.sim.setJointTargetVelocity(self.left_joint_handle, wheel_cmd_final)
+    self.sim.setJointTargetVelocity(self.right_joint_handle, wheel_cmd_final)
 
 def sysCall_cleanup():
     self.sim.setJointTargetVelocity(self.left_joint_handle, 0)
     self.sim.setJointTargetVelocity(self.right_joint_handle, 0)
-    
-    print("Self-Balancing Robot Controller Stopped")
-   
-def set_velocity_linear_set_point(vel):
-
-    self.velocity_linear_set_point = vel
-    print(f"Target velocity set to: {vel} m/s")
-
-def set_yaw(yaw_rate):
-    self.yaw = yaw_rate
-    print(f"Yaw rate set to: {yaw_rate} rad/s")
+    print("Controller stopped")
 
 def reset_controller():
-    self.velocity_angular = 0.0
-    self.velocity_linear_set_point = 0.0
-    self.yaw = 0.0
-    self.pitch_dot_filtered = 0.0
-    self.velocity_angular_filtered = 0.0
+    self.pitch_rate_filtered = 0.0
+    self.forward_velocity_filtered = 0.0
+    self.wheel_vel_avg_filtered = 0.0
+    self.target_forward_velocity = 0.0
     
     self.sim.setJointTargetVelocity(self.left_joint_handle, 0)
     self.sim.setJointTargetVelocity(self.right_joint_handle, 0)
-    
     print("Controller reset")
